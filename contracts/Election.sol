3624041616582a0794ccf799e5cfa9cff0ba8c61
Kausha Shah <kaushashah.cmpn@gmail.com>
2020-06-09 19:17:31 +0530

    Added Unit test

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,11 +1,25 @@
 pragma solidity^0.5.16;
 
 contract Election {
+	//Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
 	//store candidate
 	//read candidate
-	string public candidate;
-	//constructor
-	constructor() public{
-		candidate = "Candidate 1";
+	mapping(uint => Candidate) public candidates;
+	//store candidate count
+	uint public candidatesCount;
+
+	constructor () public{
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
+	function addCandidate(string memory _name) private{
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
