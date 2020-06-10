0c0a5b4c66408caebe85a7f3b3f887e59c6a6067
Kausha Shah <kaushashah.cmpn@gmail.com>
2020-06-10 17:31:30 +0530

    cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -7,6 +7,9 @@ contract Election {
 		string name;
 		uint voteCount;
 	}
+
+	//Store accounts that have voted
+	mapping(address => bool) public voters;
 	//store candidate
 	//read candidate
 	mapping(uint => Candidate) public candidates;
@@ -21,5 +24,18 @@ contract Election {
 	function addCandidate(string memory _name) private{
 		candidatesCount++;
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	function vote(uint _candidateId) public{
+		//require that they haven't voted before
+		require(!voters[msg.sender]);
+		//require a valid candidate 
+		require(_candidateId > 0 && _candidateId <= candidatesCount);
+		//record that voter has voted
+		voters[msg.sender] = true;
+		//update candidate vote count
+		candidates[_candidateId].voteCount++;
+		// trigger voted event
+        // votedEvent(_candidateId);
 	}
 }
